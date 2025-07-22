bf41687fb72dc879fe92f809c477adc4b751ccf0
AbzTalukdar7 <talukdar.a@hotmail.com>
2025-07-22 14:18:38 +0000

    again

--- main.tf
+++ main.tf
@@ -33,8 +33,8 @@ resource "aws_security_group" "ssh_proj" {
 
   # Inbound HTTP from anywhere
   ingress {
-    from_port = "${var.server_port}"
-    to_port = "${var.server_port}"
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
